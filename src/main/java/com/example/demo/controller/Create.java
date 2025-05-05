02da4871179bb5f0cf078a20ab23bf2b96662166
Jaeho <wogh050713@gmail.com>
2025-05-05 20:14:57 +0900

    dd

--- src/main/java/com/example/demo/controller/Create.java
+++ src/main/java/com/example/demo/controller/Create.java
@@ -1,9 +1,19 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Scanner;
+
+@RestController
 public class Create {
-@GetMapping("/");
-public 
+
+    @GetMapping("/")
+    public double Input(@RequestParam double x, @RequestParam double y) {
+
+    double result = x*y;
+    return result;
+}
 
 }
